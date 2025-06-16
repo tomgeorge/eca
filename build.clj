cb8dbb280ee0037e440502e5c2de756ad6407e8f
Eric Dallo <ericdallo06@hotmail.com>
2025-06-16 12:38:01 -0300

    fix reflection

--- build.clj
+++ build.clj
@@ -82,9 +82,7 @@
                         "--features=clj_easy.graal_build_time.InitClojureClasses"
                         (when-not (fs/windows?) "-march=compatibility")
                         "-O1"
-                        (when-not (or (:pgo-instrument opts)
-                                      (fs/windows?)) "--pgo=graalvm/default.iprof")
-                        (or (System/getenv "CLOJURE_LSP_XMX")
+                        (or (System/getenv "ECA_XMX")
                             "-J-Xmx8g")
                         (when (and linux? aarch64?)
                           ["-Djdk.lang.Process.launchMechanism=vfork"

c6547e5c6f9a6a227025d4f0523ded7b66fdab61
Eric Dallo <ericdallo06@hotmail.com>
2025-08-07 12:44:31 -0300

    tests

--- test/eca/features/prompt_test.clj
+++ test/eca/features/prompt_test.clj
@@ -25,4 +25,25 @@
       (is (string/includes? result "<repoMap description=\"Workspaces structure in a tree view, spaces represent file hierarchy\" >TREE</repoMap>"))
       (is (string/includes? result "<resource uri=\"custom://my-resource\">some-cool-content</resource>"))
       (is (string/includes? result "</contexts>"))
+      (is (string? result))))
+  (testing "Should create instructions with rules, contexts, and plan behavior"
+    (let [refined-contexts [{:type :file :path "foo.clj" :content "(ns foo)"}
+                            {:type :file :path "bar.clj" :content "(def a 1)" :partial true}
+                            {:type :repoMap}
+                            {:type :mcpResource :uri "custom://my-resource" :content "some-cool-content"}]
+          rules [{:name "rule1" :content "First rule"}
+                 {:name "rule2" :content "Second rule"}]
+          fake-repo-map (delay "TREE")
+          behavior "plan"
+          result (prompt/build-instructions refined-contexts rules fake-repo-map behavior {})]
+      (is (string/includes? result "You are ECA"))
+      (is (string/includes? result "<rules>"))
+      (is (string/includes? result "<rule name=\"rule1\">First rule</rule>"))
+      (is (string/includes? result "<rule name=\"rule2\">Second rule</rule>"))
+      (is (string/includes? result "<contexts>"))
+      (is (string/includes? result "<file path=\"foo.clj\">(ns foo)</file>"))
+      (is (string/includes? result "<file partial=true path=\"bar.clj\">...\n(def a 1)\n...</file>"))
+      (is (string/includes? result "<repoMap description=\"Workspaces structure in a tree view, spaces represent file hierarchy\" >TREE</repoMap>"))
+      (is (string/includes? result "<resource uri=\"custom://my-resource\">some-cool-content</resource>"))
+      (is (string/includes? result "</contexts>"))
       (is (string? result)))))
